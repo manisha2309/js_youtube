fba455354683f0ffee3298aceda9107b67d547ca
Manisha <156739896+manisha2309@users.noreply.github.com>
2024-08-23 07:47:50 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,16 @@
+// let score = 33
+// let score = "33"
+// let score = "33anc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Hitesh"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+//"33anc" => NaN
+// true => 1 ; false =>0
